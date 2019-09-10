e1272d64ea04a03db5822e8393311952abdd77c3
njg7716 <36088815+njg7716@users.noreply.github.com>
2019-09-10 10:50:15 -0400

    Learning
    When starting a new powershell process, the variables do not carry over

--- ET.ps1
+++ ET.ps1
@@ -1,4 +1,4 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process -WindowStyle hidden powershell "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 $port="200"
 netsh advfirewall firewall add rule name="Open Port $port" dir=in action=allow protocol=TCP localport=$port | Out-Null #adds Firewall Rule
-Start-Process -WindowStyle hidden powershell {$x = [System.Net.Sockets.TcpListener]$port; $x.Start(); Start-Sleep -Seconds (30)} | Out-Null #opens port and sleeps to keep it open
+Start-Process -WindowStyle hidden powershell {$port="200"; $x = [System.Net.Sockets.TcpListener]$port; $x.Start(); Start-Sleep -Seconds (30)} | Out-Null #opens port and sleeps to keep it open
