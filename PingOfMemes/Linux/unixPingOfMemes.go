94505c9e586d5b353a46c573c8023e74de9715e8
Nick Graca <njg7716@rit.edu>
2020-10-29 17:47:04 -0400

    For Linux, you need 10 pings in order to set it off

--- PingOfMemes/Linux/unixPingOfMemes.go
+++ PingOfMemes/Linux/unixPingOfMemes.go
@@ -9,67 +9,73 @@ import (
 )
 
 func main() {
+	var i int
+	i = 0
 	conn, err := icmp.ListenPacket("ip4:icmp", "0.0.0.0")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for {
-		msg := make([]byte, 4)
-		_, _, err := conn.ReadFrom(msg)
-		if err != nil {
-			fmt.Println(err)
-			continue
+		if i == 10 {
+			i = 0
+			msg := make([]byte, 4)
+			_, _, err := conn.ReadFrom(msg)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			//Do Things!
+			var spoopy string
+			spoopy = "               _.---._\n"
+			spoopy = spoopy + "             .'       `.\n"
+			spoopy = spoopy + "             :)       (:\n"
+			spoopy = spoopy + "             \\ (@) (@) /\n"
+			spoopy = spoopy + "              \\   A   / \n"
+			spoopy = spoopy + "               )     ( \n"
+			spoopy = spoopy + "               \\\"\"\"\"\"/ \n"
+			spoopy = spoopy + "                `._.'\n"
+			spoopy = spoopy + "                 .=.\n"
+			spoopy = spoopy + "         .---._.-.=.-._.---.\n"
+			spoopy = spoopy + "        / ':-(_.-: :-._)-:` \\ \n"
+			spoopy = spoopy + "       / /' (__.-: :-.__) `\\ \\ \n"
+			spoopy = spoopy + "      / /  (___.-` '-.___)  \\ \\ \n"
+			spoopy = spoopy + "     / /   (___.-'^`-.___)   \\ \\ \n"
+			spoopy = spoopy + "    / /    (___.-'=`-.___)    \\ \\ \n"
+			spoopy = spoopy + "   / /     (____.'=`.____)     \\ \\ \n"
+			spoopy = spoopy + "  / /       (___.'=`.___)       \\ \\ \n"
+			spoopy = spoopy + " (_.;       `---'.=.`---'       ;._)\n"
+			spoopy = spoopy + " ;||        __  _.=._  __        ||;\n"
+			spoopy = spoopy + " ;||       (  `.-.=.-.'  )       ||;\n"
+			spoopy = spoopy + " ;||       \\    `.=.'    /       ||;\n"
+			spoopy = spoopy + " ;||        \\    .=.    /        ||;\n"
+			spoopy = spoopy + " ;||       .-`.`-._.-'.'-.       ||;\n"
+			spoopy = spoopy + ".:::\\      ( ,): O O :(, )      /:::.\n"
+			spoopy = spoopy + "|||| `     / /'`--'--'`\\ \\     ' ||||\n"
+			spoopy = spoopy + "''''      / /           \\ \\      ''''\n"
+			spoopy = spoopy + "         / /             \\ \\ \n"
+			spoopy = spoopy + "        / /               \\ \\ \n"
+			spoopy = spoopy + "       / /                 \\ \\ \n"
+			spoopy = spoopy + "      / /                   \\ \\ \n"
+			spoopy = spoopy + "     / /                     \\ \\ \n"
+			spoopy = spoopy + "    /.'                       `.\\ \n"
+			spoopy = spoopy + "   (_)'                       `(_)\n"
+			spoopy = spoopy + "    \\\\.                       .//\n"
+			spoopy = spoopy + "     \\\\.                     .//\n"
+			spoopy = spoopy + "      \\\\.                   .//\n"
+			spoopy = spoopy + "       \\\\.                 .//\n"
+			spoopy = spoopy + "        \\\\.               .//\n"
+			spoopy = spoopy + "         \\\\.             .//\n"
+			spoopy = spoopy + "          \\\\.           .//\n"
+			spoopy = spoopy + "          ///)         (\\\\\\ \n"
+			spoopy = spoopy + "        ,///'           `\\\\\\, \n"
+			spoopy = spoopy + "       ///'               `\\\\\\ \n"
+			spoopy = spoopy + "      \"\"'                   '\"\"\n"
+			spoopy = spoopy + "Spooky Scary Skeletons Send Shivers Down Your Spine!\n"
+			fmt.Println("Ping!")
+			cmd := exec.Command("sudo", "wall", spoopy)
+			err = cmd.Run()
 		}
-		//Do Things!
-		var spoopy string
-		spoopy = "               _.---._\n"
-		spoopy = spoopy + "             .'       `.\n"
-		spoopy = spoopy + "             :)       (:\n"
-		spoopy = spoopy + "             \\ (@) (@) /\n"
-		spoopy = spoopy + "              \\   A   / \n"
-		spoopy = spoopy + "               )     ( \n"
-		spoopy = spoopy + "               \\\"\"\"\"\"/ \n"
-		spoopy = spoopy + "                `._.'\n"
-		spoopy = spoopy + "                 .=.\n"
-		spoopy = spoopy + "         .---._.-.=.-._.---.\n"
-		spoopy = spoopy + "        / ':-(_.-: :-._)-:` \\ \n"
-		spoopy = spoopy + "       / /' (__.-: :-.__) `\\ \\ \n"
-		spoopy = spoopy + "      / /  (___.-` '-.___)  \\ \\ \n"
-		spoopy = spoopy + "     / /   (___.-'^`-.___)   \\ \\ \n"
-		spoopy = spoopy + "    / /    (___.-'=`-.___)    \\ \\ \n"
-		spoopy = spoopy + "   / /     (____.'=`.____)     \\ \\ \n"
-		spoopy = spoopy + "  / /       (___.'=`.___)       \\ \\ \n"
-		spoopy = spoopy + " (_.;       `---'.=.`---'       ;._)\n"
-		spoopy = spoopy + " ;||        __  _.=._  __        ||;\n"
-		spoopy = spoopy + " ;||       (  `.-.=.-.'  )       ||;\n"
-		spoopy = spoopy + " ;||       \\    `.=.'    /       ||;\n"
-		spoopy = spoopy + " ;||        \\    .=.    /        ||;\n"
-		spoopy = spoopy + " ;||       .-`.`-._.-'.'-.       ||;\n"
-		spoopy = spoopy + ".:::\\      ( ,): O O :(, )      /:::.\n"
-		spoopy = spoopy + "|||| `     / /'`--'--'`\\ \\     ' ||||\n"
-		spoopy = spoopy + "''''      / /           \\ \\      ''''\n"
-		spoopy = spoopy + "         / /             \\ \\ \n"
-		spoopy = spoopy + "        / /               \\ \\ \n"
-		spoopy = spoopy + "       / /                 \\ \\ \n"
-		spoopy = spoopy + "      / /                   \\ \\ \n"
-		spoopy = spoopy + "     / /                     \\ \\ \n"
-		spoopy = spoopy + "    /.'                       `.\\ \n"
-		spoopy = spoopy + "   (_)'                       `(_)\n"
-		spoopy = spoopy + "    \\\\.                       .//\n"
-		spoopy = spoopy + "     \\\\.                     .//\n"
-		spoopy = spoopy + "      \\\\.                   .//\n"
-		spoopy = spoopy + "       \\\\.                 .//\n"
-		spoopy = spoopy + "        \\\\.               .//\n"
-		spoopy = spoopy + "         \\\\.             .//\n"
-		spoopy = spoopy + "          \\\\.           .//\n"
-		spoopy = spoopy + "          ///)         (\\\\\\ \n"
-		spoopy = spoopy + "        ,///'           `\\\\\\, \n"
-		spoopy = spoopy + "       ///'               `\\\\\\ \n"
-		spoopy = spoopy + "      \"\"'                   '\"\"\n"
-		spoopy = spoopy + "Spooky Scary Skeletons Send Shivers Down Your Spine!\n"
-		fmt.Println("Ping!")
-		cmd := exec.Command("sudo", "wall", spoopy)
-		err = cmd.Run()
+		i++
 	}
 }
